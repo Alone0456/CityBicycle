ced11cb9939241b864298d3093e4d8df6195f302
lizhichao <2658085067@qq.com>
2024-03-15 18:35:25 +0800

    add: Bicycle.java

---
+++ cityBicycle-common/src/main/java/com/powernode/lzc/domain/entity/Bicycle.java
@@ -0,0 +1,24 @@
+package com.powernode.lzc.domain.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Builder
+@TableName("t_bicycle")
+public class Bicycle {
+  @TableId
+  private long bicycleId;
+  private long bicycleType;
+  private long rentNum;
+  private Date createTime;
+  private long bicycleStatus;
+  private long stationId;
+  private String stationName;
+  private Date inspectTime;
+  private long inspectNum;
+}
